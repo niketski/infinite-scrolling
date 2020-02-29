49ef632291ee70f77993d5763ded8a2c914c65c8
Nicole Benedict <lim.nicolebenedict3@gmail.com>
2020-02-29 18:02:16 +0800

    set up done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,3 @@ import './styles/bootstrap.scss';
 import './styles/main.scss';
 import '../node_modules/font-awesome/css/font-awesome.css';
 
-console.log('hello world');
-
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-let asd = new Person('nico');
